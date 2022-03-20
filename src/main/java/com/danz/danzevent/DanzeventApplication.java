acde734d767d79d22c58ebd61842a1e6a6449c79
Karel Muñiz <oricha@gmail.com>
2022-03-20 20:33:29 +0100

    First code

---
+++ src/main/java/com/danz/danzevent/DanzeventApplication.java
@@ -0,0 +1,13 @@
+package com.danz.danzevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanzeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanzeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danz/danzevent/DanzeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danz.danzevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanzeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
