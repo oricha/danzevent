160e1242eedf2ca463fd8359142ae88fc865c942
oricha <oricha@gmail.com>
2023-12-20 00:41:30 +0100

    Add Spring security and thymeleaf

---
+++ src/main/java/com/danz/danzevent/configuration/SecurityFilterChain.java
@@ -0,0 +1,23 @@
+package com.danz.danzevent.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilterChain {
+
+    @Bean
+    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(expressionInterceptUrlRegistry ->
+                        expressionInterceptUrlRegistry
+                                .anyRequest()
+                                .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
